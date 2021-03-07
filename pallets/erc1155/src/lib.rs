165303cb2ebf7fb5063e178aebce1ef66e5ef8db
wjsxqs <xsmlcp@163.com>
2021-03-07 16:08:22 +0800

    Remove create mint and burn from erc1155 call

--- pallets/erc1155/src/lib.rs
+++ pallets/erc1155/src/lib.rs
@@ -161,20 +161,6 @@ pub mod pallet {
         }
 
         #[pallet::weight(10_000)]
-        pub fn create_token(
-            origin: OriginFor<T>,
-            instance_id: T::InstanceId,
-            token_id: T::TokenId,
-            is_nf: bool,
-            uri: Vec<u8>,
-        ) -> DispatchResultWithPostInfo {
-            let who = ensure_signed(origin)?;
-
-            Self::do_create_token(&who, instance_id, token_id, is_nf, uri)?;
-            Ok(().into())
-        }
-
-        #[pallet::weight(10_000)]
         pub fn set_approval_for_all(
             origin: OriginFor<T>,
             operator: T::AccountId,
@@ -219,66 +205,6 @@ pub mod pallet {
             let _who = ensure_signed(origin)?;
 
             Self::do_batch_transfer_from(&from, &to, instance_id, token_ids, amounts)?;
-
-            Ok(().into())
-        }
-
-        #[pallet::weight(10_000)]
-        pub fn mint(
-            origin: OriginFor<T>,
-            to: T::AccountId,
-            instance_id: T::InstanceId,
-            token_id: T::TokenId,
-            amount: Balance,
-        ) -> DispatchResultWithPostInfo {
-            let _who = ensure_signed(origin)?;
-
-            Self::do_mint(&to, instance_id, token_id, amount)?;
-
-            Ok(().into())
-        }
-
-        #[pallet::weight(10_000)]
-        pub fn batch_mint(
-            origin: OriginFor<T>,
-            to: T::AccountId,
-            instance_id: T::InstanceId,
-            token_ids: Vec<T::TokenId>,
-            amounts: Vec<Balance>,
-        ) -> DispatchResultWithPostInfo {
-            let _who = ensure_signed(origin)?;
-
-            Self::do_batch_mint(&to, instance_id, token_ids, amounts)?;
-
-            Ok(().into())
-        }
-
-        #[pallet::weight(10_000)]
-        pub fn burn(
-            origin: OriginFor<T>,
-            from: T::AccountId,
-            instance_id: T::InstanceId,
-            token_id: T::TokenId,
-            amount: Balance,
-        ) -> DispatchResultWithPostInfo {
-            let _who = ensure_signed(origin)?;
-
-            Self::do_burn(&from, instance_id, token_id, amount)?;
-
-            Ok(().into())
-        }
-
-        #[pallet::weight(10_000)]
-        pub fn batch_burn(
-            origin: OriginFor<T>,
-            from: T::AccountId,
-            instance_id: T::InstanceId,
-            token_ids: Vec<T::TokenId>,
-            amounts: Vec<Balance>,
-        ) -> DispatchResultWithPostInfo {
-            let _who = ensure_signed(origin)?;
-
-            Self::do_batch_burn(&from, instance_id, token_ids, amounts)?;
 
             Ok(().into())
         }
