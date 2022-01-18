b821b6311bb5cb48ee030fe823d4cc20a50272f9
wjsxqs <xsmlcp@163.com>
2022-01-18 11:12:29 +0800

    Update copyright 2022

--- pallets/exchange/src/tests.rs
+++ pallets/exchange/src/tests.rs
@@ -1,6 +1,6 @@
 // This file is part of Web3Games.
 
-// Copyright (C) 2021 Web3Games https://web3games.org
+// Copyright (C) 2021-2022 Web3Games https://web3games.org
 // SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
 
 // This program is free software: you can redistribute it and/or modify
