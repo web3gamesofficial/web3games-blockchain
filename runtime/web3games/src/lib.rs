cca085509acfa88349a3569b1ff769ed2a008a82
henryliu <17600047539@163.com>
2022-03-18 14:38:55 +0800

    update

--- runtime/web3games/src/lib.rs
+++ runtime/web3games/src/lib.rs
@@ -380,7 +380,7 @@ impl FeeCalculator for FixedGasPrice {
 }
 
 parameter_types! {
-	pub const ChainId: u64 = 1337;
+	pub const ChainId: u64 = 105;
 	pub BlockGasLimit: U256 = U256::from(u32::max_value());
 	pub PrecompilesValue: Web3GamesPrecompiles<Runtime> = Web3GamesPrecompiles::<_>::new();
 }
