ef4fdc3258216c53f58284cb0fd545ccf794462d
wjsxqs <xsmlcp@163.com>
2022-04-13 17:44:31 +0800

    Remove unused

--- runtime/web3games/src/lib.rs
+++ runtime/web3games/src/lib.rs
@@ -41,7 +41,7 @@ use sp_runtime::{
 	create_runtime_str, generic, impl_opaque_keys,
 	traits::{
 		AccountIdConversion, AccountIdLookup, BlakeTwo256, Block as BlockT, Dispatchable,
-		NumberFor, PostDispatchInfoOf, Zero,
+		NumberFor, PostDispatchInfoOf,
 	},
 	transaction_validity::{TransactionSource, TransactionValidity, TransactionValidityError},
 	ApplyExtrinsicResult, Percent,
