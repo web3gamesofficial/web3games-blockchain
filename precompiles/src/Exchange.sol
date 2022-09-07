f5b2b726ba1baad7aed05fd26c5bc53ec736b5fc
NingBo Wang <2536935847@qq.com>
2022-09-07 16:38:18 +0800

    update exchange rpc

--- precompiles/src/Exchange.sol
+++ precompiles/src/Exchange.sol
@@ -5,7 +5,11 @@ pragma solidity ^0.8.0;
 interface Exchange {
     function create_pool(uint256 token_a,uint256 token_b) external;
     function add_liquidity(uint256 token_a,uint256 token_b,uint256 amount_a_desired,uint256 amount_b_desired,uint256 amount_a_min,uint256 amount_b_min,address to,uint256 deadline) external;
+    function add_liquidity_w3g(uint256 token,uint256 amount_w3g_desired,uint256 amount_desired,uint256 amount_w3g_min,uint256 amount_min,address to,uint256 deadline) external;
     function remove_liquidity(uint256 token_a,uint256 token_b,uint256 liquidity,uint256 amount_a_min,uint256 amount_b_min,address to,uint256 deadline) external;
+    function remove_liquidity_w3g(uint256 token,uint256 liquidity,uint256 amount_w3g_min,uint256 amount_min,address to,uint256 deadline) external;
     function swap_exact_tokens_for_tokens(uint256 amount_in,uint256 amount_out_min,uint256[] memory path,address to,uint256 deadline) external;
+    function swap_exact_w3g_for_tokens(uint256 amount_in_w3g,uint256 amount_out_min,uint256[] memory path,address to,uint256 deadline) external;
     function swap_tokens_for_exact_tokens(uint256 amount_out,uint256 amount_in_max,uint256[] memory path,address to,uint256 deadline) external;
+    function swap_tokens_for_exact_w3g(uint256 amount_out_w3g,uint256 amount_in_max,uint256[] memory path,address to,uint256 deadline) external;
 }
--- precompiles/src/exchange.rs
+++ precompiles/src/exchange.rs
@@ -31,11 +31,15 @@ pub type FungibleTokenIdOf<Runtime> = <Runtime as pallet_token_fungible::Config>
 enum Action {
 	CreatePool = "create_pool(uint256,uint256)",
 	AddLiquidity = "add_liquidity(uint256,uint256,uint256,uint256,uint256,uint256,address,uint256)",
+	AddLiquidityW3G = "add_liquidity_w3g(uint256,uint256,uint256,uint256,uint256,address,uint256)",
 	RemoveLiquidity = "remove_liquidity(uint256,uint256,uint256,uint256,uint256,address,uint256)",
+	RemoveLiquidityW3G = "remove_liquidity_w3g(uint256,uint256,uint256,uint256,address,uint256)",
 	SwapExactTokensForTokens =
 		"swap_exact_tokens_for_tokens(uint256,uint256,uint256[],address,uint256)",
+	SwapExactW3GForTokens = "swap_exact_w3g_for_tokens(uint256,uint256,uint256[],address,uint256)",
 	SwapTokensForExactTokens =
 		"swap_tokens_for_exact_tokens(uint256,uint256,uint256[],address,uint256)",
+	SwapTokensForExactW3G = "swap_tokens_for_exact_w3g(uint256,uint256,uint256[],address,uint256)",
 }
 
 pub struct ExchangeExtension<Runtime>(PhantomData<Runtime>);
@@ -58,18 +62,26 @@ where
 			if let Err(err) = handle.check_function_modifier(match selector {
 				Action::CreatePool |
 				Action::AddLiquidity |
+				Action::AddLiquidityW3G |
 				Action::RemoveLiquidity |
+				Action::RemoveLiquidityW3G |
 				Action::SwapExactTokensForTokens |
-				Action::SwapTokensForExactTokens => FunctionModifier::NonPayable,
+				Action::SwapExactW3GForTokens |
+				Action::SwapTokensForExactTokens |
+				Action::SwapTokensForExactW3G => FunctionModifier::NonPayable,
 			}) {
 				return Some(Err(err))
 			}
 			match selector {
 				Action::CreatePool => Self::create_pool(handle),
 				Action::AddLiquidity => Self::add_liquidity(handle),
+				Action::AddLiquidityW3G => Self::add_liquidity_w3g(handle),
 				Action::RemoveLiquidity => Self::remove_liquidity(handle),
+				Action::RemoveLiquidityW3G => Self::remove_liquidity_w3g(handle),
 				Action::SwapExactTokensForTokens => Self::swap_exact_tokens_for_tokens(handle),
+				Action::SwapExactW3GForTokens => Self::swap_exact_w3g_for_tokens(handle),
 				Action::SwapTokensForExactTokens => Self::swap_tokens_for_exact_tokens(handle),
+				Action::SwapTokensForExactW3G => Self::swap_tokens_for_exact_w3g(handle),
 			}
 		};
 		Some(result)
@@ -113,7 +125,7 @@ where
 	}
 	fn add_liquidity(handle: &mut impl PrecompileHandle) -> EvmResult<PrecompileOutput> {
 		let mut input = EvmDataReader::new_skip_selector(handle.input())?;
-		input.expect_arguments(7)?;
+		input.expect_arguments(8)?;
 		let token_a: FungibleTokenIdOf<Runtime> = input.read::<u128>()?.into();
 		let token_b: FungibleTokenIdOf<Runtime> = input.read::<u128>()?.into();
 		let amount_a_desired: Balance = input.read::<u128>()?.into();
@@ -137,6 +149,38 @@ where
 					amount_b_desired,
 					amount_a_min,
 					amount_b_min,
+					to,
+					deadline,
+				},
+			)?;
+		}
+
+		Ok(succeed(EvmDataWriter::new().write(true).build()))
+	}
+	fn add_liquidity_w3g(handle: &mut impl PrecompileHandle) -> EvmResult<PrecompileOutput> {
+		let mut input = EvmDataReader::new_skip_selector(handle.input())?;
+		input.expect_arguments(7)?;
+		let token: FungibleTokenIdOf<Runtime> = input.read::<u128>()?.into();
+		let amount_w3g_desired: Balance = input.read::<u128>()?.into();
+		let amount_desired: Balance = input.read::<u128>()?.into();
+		let amount_w3g_min: Balance = input.read::<u128>()?.into();
+		let amount_min: Balance = input.read::<u128>()?.into();
+		let to: H160 = input.read::<Address>()?.into();
+		let deadline = input.read::<BlockNumber>()?.into();
+		let to: Runtime::AccountId = Runtime::AddressMapping::into_account_id(to);
+		{
+			// Build call with origin.
+			let origin = Runtime::AddressMapping::into_account_id(handle.context().caller);
+			// Dispatch call (if enough gas).
+			RuntimeHelper::<Runtime>::try_dispatch(
+				handle,
+				Some(origin).into(),
+				pallet_exchange::Call::<Runtime>::add_liquidity_w3g {
+					token,
+					amount_w3g_desired,
+					amount_desired,
+					amount_w3g_min,
+					amount_min,
 					to,
 					deadline,
 				},
@@ -169,6 +213,36 @@ where
 					liquidity,
 					amount_a_min,
 					amount_b_min,
+					to,
+					deadline,
+				},
+			)?;
+		}
+
+		Ok(succeed(EvmDataWriter::new().write(true).build()))
+	}
+	fn remove_liquidity_w3g(handle: &mut impl PrecompileHandle) -> EvmResult<PrecompileOutput> {
+		let mut input = EvmDataReader::new_skip_selector(handle.input())?;
+		input.expect_arguments(6)?;
+		let token: FungibleTokenIdOf<Runtime> = input.read::<u128>()?.into();
+		let liquidity: Balance = input.read::<u128>()?.into();
+		let amount_w3g_min: Balance = input.read::<u128>()?.into();
+		let amount_min: Balance = input.read::<u128>()?.into();
+		let to: H160 = input.read::<Address>()?.into();
+		let deadline = input.read::<BlockNumber>()?.into();
+		let to: Runtime::AccountId = Runtime::AddressMapping::into_account_id(to);
+		{
+			// Build call with origin.
+			let origin = Runtime::AddressMapping::into_account_id(handle.context().caller);
+			// Dispatch call (if enough gas).
+			RuntimeHelper::<Runtime>::try_dispatch(
+				handle,
+				Some(origin).into(),
+				pallet_exchange::Call::<Runtime>::remove_liquidity_w3g {
+					token,
+					liquidity,
+					amount_w3g_min,
+					amount_min,
 					to,
 					deadline,
 				},
@@ -211,6 +285,40 @@ where
 
 		Ok(succeed(EvmDataWriter::new().write(true).build()))
 	}
+	fn swap_exact_w3g_for_tokens(
+		handle: &mut impl PrecompileHandle,
+	) -> EvmResult<PrecompileOutput> {
+		let mut input = EvmDataReader::new_skip_selector(handle.input())?;
+		input.expect_arguments(5)?;
+		let amount_in_w3g: Balance = input.read::<u128>()?.into();
+		let amount_out_min: Balance = input.read::<u128>()?.into();
+		let u128_path = input.read::<Vec<u128>>()?;
+		let mut path: Vec<FungibleTokenIdOf<Runtime>> = vec![];
+		for i in 0..u128_path.len() {
+			path.push(u128_path[i].into())
+		}
+		let to: H160 = input.read::<Address>()?.into();
+		let deadline = input.read::<BlockNumber>()?.into();
+		let to: Runtime::AccountId = Runtime::AddressMapping::into_account_id(to);
+		{
+			// Build call with origin.
+			let origin = Runtime::AddressMapping::into_account_id(handle.context().caller);
+			// Dispatch call (if enough gas).
+			RuntimeHelper::<Runtime>::try_dispatch(
+				handle,
+				Some(origin).into(),
+				pallet_exchange::Call::<Runtime>::swap_exact_w3g_for_tokens {
+					amount_in_w3g,
+					amount_out_min,
+					path,
+					to,
+					deadline,
+				},
+			)?;
+		}
+
+		Ok(succeed(EvmDataWriter::new().write(true).build()))
+	}
 	fn swap_tokens_for_exact_tokens(
 		handle: &mut impl PrecompileHandle,
 	) -> EvmResult<PrecompileOutput> {
@@ -235,6 +343,40 @@ where
 				Some(origin).into(),
 				pallet_exchange::Call::<Runtime>::swap_tokens_for_exact_tokens {
 					amount_out,
+					amount_in_max,
+					path,
+					to,
+					deadline,
+				},
+			)?;
+		}
+
+		Ok(succeed(EvmDataWriter::new().write(true).build()))
+	}
+	fn swap_tokens_for_exact_w3g(
+		handle: &mut impl PrecompileHandle,
+	) -> EvmResult<PrecompileOutput> {
+		let mut input = EvmDataReader::new_skip_selector(handle.input())?;
+		input.expect_arguments(5)?;
+		let amount_out_w3g: Balance = input.read::<u128>()?.into();
+		let amount_in_max: Balance = input.read::<u128>()?.into();
+		let u128_path = input.read::<Vec<u128>>()?;
+		let mut path: Vec<FungibleTokenIdOf<Runtime>> = vec![];
+		for i in 0..u128_path.len() {
+			path.push(u128_path[i].into())
+		}
+		let to: H160 = input.read::<Address>()?.into();
+		let deadline = input.read::<BlockNumber>()?.into();
+		let to: Runtime::AccountId = Runtime::AddressMapping::into_account_id(to);
+		{
+			// Build call with origin.
+			let origin = Runtime::AddressMapping::into_account_id(handle.context().caller);
+			// Dispatch call (if enough gas).
+			RuntimeHelper::<Runtime>::try_dispatch(
+				handle,
+				Some(origin).into(),
+				pallet_exchange::Call::<Runtime>::swap_tokens_for_exact_w3g {
+					amount_out_w3g,
 					amount_in_max,
 					path,
 					to,
