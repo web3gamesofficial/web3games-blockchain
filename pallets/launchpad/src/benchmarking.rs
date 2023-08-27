671adcb463cab69368c2605b9d4a2a3fedb28031
Siddharth Teli <telisiddharth123@yahoo.in>
2023-08-27 14:49:37 +0530

    Add: verify in launchpad pallet benchmark.

--- pallets/launchpad/src/benchmarking.rs
+++ pallets/launchpad/src/benchmarking.rs
@@ -15,6 +15,11 @@ const USDT: u128 = 2;
 const W3G_DECIMALS: u128 = 1_000_000_000_000_000_000;
 const USDT_DECIMALS: u128 = 1_000_000;
 
+// Helper to assert last event.
+fn assert_last_event<T: Config>(generic_event: <T as Config>::Event) {
+	frame_system::Pallet::<T>::assert_last_event(generic_event.into());
+}
+
 fn setup<T: Config>() -> DispatchResult {
 	let alice: T::AccountId = account("alice", 0, 0);
 	let bob: T::AccountId = account("bob", 0, 0);
@@ -53,7 +58,11 @@ benchmarks! {
 		let alice: T::AccountId = account("alice", 0, 0);
 		setup::<T>()?;
 	}: _(RawOrigin::Signed(alice),T::BlockNumber::from(10u32),T::BlockNumber::from(10u32),W3G,USDT,10 * W3G_DECIMALS,1 * USDT_DECIMALS)
-
+	verify {
+		assert_eq!(NextPoolId::<T>::get(), 1);
+		assert!(Pools::<T>::get(0).is_some());
+		assert_last_event::<T>(Event::<T>::PoolCreated(0).into());
+	}
 	buy_token {
 		let alice: T::AccountId = account("alice", 0, 0);
 		let bob: T::AccountId = account("bob", 0, 0);
@@ -68,7 +77,15 @@ benchmarks! {
 				1 * USDT_DECIMALS,
 		));
 		System::<T>::set_block_number(T::BlockNumber::from(10u32));
-	}: _(RawOrigin::Signed(bob),0,2)
+	}: _(RawOrigin::Signed(bob.clone()), 0, 2)
+	verify {
+		assert_eq!(AccountPoolIdLocked::<T>::get((bob.clone(), 0)), Some(ClaimInfo {
+			balance: 2 * W3G_DECIMALS,
+			is_claimed: false,
+		}));
+		assert_eq!(Pools::<T>::get(0).unwrap().raise_amount, 8 * W3G_DECIMALS);
+		assert_last_event::<T>(Event::<T>::BuyToken(bob, 0, 2 * W3G_DECIMALS).into());
+	}
 
 	claim {
 		let alice: T::AccountId = account("alice", 0, 0);
@@ -90,7 +107,19 @@ benchmarks! {
 				2
 		));
 		System::<T>::set_block_number(T::BlockNumber::from(21u32));
-	}: _(RawOrigin::Signed(bob),0)
+	}: _(RawOrigin::Signed(bob.clone()),0)
+	verify {
+		assert_eq!(AccountPoolIdLocked::<T>::get((bob.clone(), 0)), Some(ClaimInfo {
+			balance: 2 * W3G_DECIMALS,
+			is_claimed: true,
+		}));
+		assert_eq!(Pools::<T>::get(0).unwrap().raise_amount, 8 * W3G_DECIMALS);
+		assert_last_event::<T>(Event::<T>::Claim(
+			bob,
+			0,
+			2 * W3G_DECIMALS,
+		).into());
+	}
 
 	owner_claim {
 		let alice: T::AccountId = account("alice", 0, 0);
@@ -106,7 +135,17 @@ benchmarks! {
 				1 * USDT_DECIMALS,
 		));
 		System::<T>::set_block_number(T::BlockNumber::from(21u32));
-	}: _(RawOrigin::Signed(alice),0)
+	}: _(RawOrigin::Signed(alice.clone()),0)
+	verify {
+		assert_eq!(AccountPoolIdLocked::<T>::get((bob.clone(), 0)), None);
+		assert_eq!(Pools::<T>::get(0).unwrap().raise_amount, 10 * W3G_DECIMALS);
+		assert_last_event::<T>(Event::<T>::OwnerClaim(
+			alice,
+			0,
+			10 * W3G_DECIMALS,
+			0,
+		).into());
+	}
 
 	impl_benchmark_test_suite!(Launchpad, crate::mock::new_test_ext(), crate::mock::Test);
 }
